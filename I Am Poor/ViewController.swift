7cb30c21641318ebbb6137207b30c64bd03dad5b
Naomi Matthews <67332327+naomimatthews@users.noreply.github.com>
2023-06-28 22:19:06 +0100

    App Added

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Naomi Matthews on 15/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
